237f10c50bd73271dc6aaf8b9d07aa62c2cacf98
brucecrust <brucecrusty@gmail.com>
2021-08-25 18:33:15 -0600

    Added resources, added slots for New, Open, and Save as

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,12 +2,58 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow) {
+    : QMainWindow(parent),
+      ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
 }
 
 MainWindow::~MainWindow() {
     delete ui;
 }
 
+// Slot Methods:
+void MainWindow::on_actionNew_triggered() {
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+void MainWindow::on_actionOpen_triggered() {
+    QString filename = QFileDialog::getOpenFileName(this, "Open File");
+    QFile file(filename);
+    currentFile = filename;
+
+    if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+        return;
+    }
+
+    setWindowTitle(filename);
+
+    QTextStream in(&file);
+    QString text = in.readAll();
+
+    ui->textEdit->setText(text);
+
+    file.close();
+}
+
+void MainWindow::on_actionSave_triggered() {
+    QString filename = QFileDialog::getSaveFileName(this, "Save File");
+    QFile file(filename);
+    currentFile = file;
+
+    if (!file.open(QIODevice::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+
+    setWindowTitle(filename);
+
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+
+    out << text;
+
+    file.close();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QPrinter>
+#include <QPrintDialog>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -11,10 +17,19 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
+    // Constructor / Destructor
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QString currentFile = "";
 };
 #endif // MAINWINDOW_H
